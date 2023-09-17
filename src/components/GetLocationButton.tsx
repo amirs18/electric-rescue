686c8882b926834add445436c71ead7e2d0a2e34
amirs18 <amir.shafat1@gmail.com>
2023-09-18 01:54:34 +0300

    check location premissions

--- src/components/GetLocationButton.tsx
+++ src/components/GetLocationButton.tsx
@@ -1,14 +1,40 @@
-export default function GetLocationButton  () {
-  if (navigator ){
+import { useState } from "react";
 
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude, position.coords.longitude);
-      });
+export default function GetLocationButton() {
+  const [prmessions, setPrmessions] = useState(false);
+
+  if ("geolocation" in navigator) {
+    if (navigator.geolocation) {
+      console.log(navigator.geolocation);
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          if (!prmessions) setPrmessions(true);
+          console.log(position.coords.latitude, position.coords.longitude);
+        },
+        (error) => {
+          if (error.code === error.PERMISSION_DENIED) {
+            setPrmessions(false);
+          }
+        }
+      );
     } else {
-      /* geolocation IS NOT available */
+    }
+    if (!prmessions) {
+      return (
+        <dialog open id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">hi</p>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      );
     }
   }
-return <div className="btn" >loc</div>
-
 }
