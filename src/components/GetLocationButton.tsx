057db20eaed55878483bb32ec856011e23c89c5d
unknown <amir.shafat1@gmail.com>
2023-09-17 18:28:06 +0300

    tailwind

--- src/components/GetLocationButton.tsx
+++ src/components/GetLocationButton.tsx
@@ -1,11 +1,14 @@
 export default function GetLocationButton  () {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords.latitude, position.coords.longitude);
-    });
-  } else {
-    /* geolocation IS NOT available */
+  if (navigator ){
+
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+      });
+    } else {
+      /* geolocation IS NOT available */
+    }
   }
-return <div>loc</div>
+return <div className="btn" >loc</div>
 
 }
